945bee6ac0e9e99acd56fadf02649a644fb31a63
Priit <prpall@ttu.ee>
2019-02-11 16:59:05 +0200

    added path tracking on map

--- lab2/Maze.py
+++ lab2/Maze.py
@@ -55,27 +55,6 @@ lava_map3 = [
 ]
 
 
-def read_map(map):
-    start_row = 0
-    start_col = 0
-    width = 0
-    height = 0
-
-    for row in map:
-        height += 1
-        if "s" in row:
-            start_row = height - 1
-            start_col = row.index("s")
-        if "D" in row:
-            print(height - 1, ",", row.index("D"))
-        width = len(row)
-
-    print("We have a " + str(width) + "x" + str(height) + " sized maze.")
-    #find_neighbours((start_row - 1, start_col), width, height)
-    #find_neighbours(lava_map1, (6, 10), width, height)
-    #print("Jou - " + map[14][16])
-
-
 def find_neighbours(map, location, width, height):
     row = location[0]
     col = location[1]
@@ -102,22 +81,19 @@ def find_neighbours(map, location, width, height):
         possible_moves.append((row, col + 1))
         move_names.append("east")
 
-    #print(move_names)
-    #print("Possible moves: " + str(possible_moves))
-    #print("row: " + str(row) + "\ncolumn: " + str(col))
     return possible_moves
 
-# -----------------------------------------------------------
-def minu_otsing(kaart):
+
+def my_search(map):
     start_row = 0
     start_col = 0
     width = 0
     height = 0
     diamond = ()
     path = []
-    map_with_path = list(kaart)
+    map_with_path = list(map)
 
-    for row in kaart:
+    for row in map:
         height += 1
         if "s" in row:
             start_row = height - 1
@@ -126,8 +102,6 @@ def minu_otsing(kaart):
 
     start = (start_row, start_col)
 
-    # start (algolek) on näiteks tuple kujul (x, y)
-
     frontier = Queue()
     frontier.put(start)
     came_from = {}
@@ -135,45 +109,31 @@ def minu_otsing(kaart):
 
     while not frontier.empty():
         current = frontier.get()
-        if kaart[current[0]][current[1]] == "D":
+        if map[current[0]][current[1]] == "D":
             print("Found the diamond!")
             diamond = current
             print(diamond)
             while current != start:
                 path.append(current)
                 temp = list(map_with_path[current[0]])
-                temp[current[1]] = "@"
-                "".join(temp)
-                map_with_path[current[0]] = temp
-                #map_with_path[current[0]][current[1]] = "X"
+                if temp[current[1]] != "D":
+                    temp[current[1]] = "."
+                    temp = "".join(temp)
+                    map_with_path[current[0]] = temp
                 current = came_from[current]
             path.append(start)
             path.reverse()
             break
-
-        # meid ei huvita kõik teed, seega peaks kontrollima, kas current on teemant.
-        # Kui on, siis katkestame otsingu
-        # (ja loomulikult jätame teemandi koordinaadid meelde)
-
-        for next in find_neighbours(kaart, current, width, height):  # see osa tuleb suht palju ümber teha.
-                                               # tuleb leida sobivad naaberruudud kaardilt
-                                               # nagu ta meile ette on antud (ülal, all,
-                                               # paremal ja vasakul olev ruut)
+        for next in find_neighbours(map, current, width, height):
             if next not in came_from:
                 frontier.put(next)
                 came_from[next] = current
 
-
-    # Kui teemant on leitud, tuleb ka teekond rekonstrueerida
-    # mis andmestruktuurina teekonda esitada, pole oluline,
-    # aga loomulik viis oleks list
     print(path)
-    #for row in map_with_path:
-        #print(row)
-        #print("\n")
+    for row in map_with_path:
+        print(row)
     return path
 
 
 if __name__ == '__main__':
-    #read_map(lava_map1)
-    minu_otsing(lava_map1)
+    my_search(lava_map1)
